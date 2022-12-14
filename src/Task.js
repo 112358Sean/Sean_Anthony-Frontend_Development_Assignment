4e9d1f8f99a9180dd87268e8d1c29b8db9788201
112358Sean <seanthony99@gmail.com>
2022-12-14 15:08:24 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Sean Anthony" // TODO: replace this
+const studentId = "FE2664210" // TODO: replace this
+
+export { url, studentName, studentId }
