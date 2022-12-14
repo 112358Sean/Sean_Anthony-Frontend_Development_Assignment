b0abaa1d99b7e6aa1eed8c6517e66547e4d83ad8
112358Sean <seanthony99@gmail.com>
2022-12-14 15:13:01 +0700

    updating Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://joyful-marigold-1668a1.netlify.app" // TODO: replace this
 const studentName = "Sean Anthony" // TODO: replace this
 const studentId = "FE2664210" // TODO: replace this
 
